3eeada35d82b7d706625af752aa80579e825c45b
chudnofskyATD <mpereannor@gmail.com>
2020-02-12 13:02:16 +0000

    Room class created

--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name, description): 
+    self.name = name 
+    self.description = description
+    
+  def __str__(self):
+    info = ''
+    info  += f'{self.name} \n{self.description} \n'
+    return info
+    
+    
+house = Room('mansion', 'extremely luxurious')
+
+print(house)
